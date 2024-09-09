d80f3ca6aae9b6e8c93ada76e64dc5cfec48d4e5
ErgiIsmahili <ergi.ismahili@gmail.com>
2024-09-08 20:36:16 -0400

    F - Added Appbar with title.

--- lib/main.dart
+++ lib/main.dart
@@ -8,44 +8,37 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Restaurant App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const RestaurantPage(),
+      home: const RestaurantListScreen(),
     );
   }
 }
 
-  class RestaurantPage extends ConsumerWidget {
-    const RestaurantPage({super.key});
+class RestaurantListScreen extends ConsumerWidget {
+  const RestaurantListScreen({super.key});
 
-    @override
-    Widget build(BuildContext context, WidgetRef ref) {
-      return const Scaffold(
-        body: Center(
-          child: Text("Restaurants"),
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+
+    return Scaffold(
+  appBar: AppBar(
+    title: const Center(
+      child: Text(
+        'Restaurants',
+        style: TextStyle(
+          fontSize: 32, 
         ),
-      );
-    }
+      ),
+    ),
+    toolbarHeight: 80, 
+  ),
+);
+
+  }
 }
